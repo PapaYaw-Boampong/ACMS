f32f0d2183f69ea90f276a42795ed35ece3269ca
akooku <92333319+akooku@users.noreply.github.com>
2024-08-08 00:21:49 +0000

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -18,7 +18,7 @@ class SpecialHeader extends HTMLElement {
                                         <span>Search</span>
                                     </div>
                                 </a>
-                                <a href="login.php" class="widget-header me-4 text-dark m-none">
+                                <a href="../login/login.php" class="widget-header me-4 text-dark m-none">
                                     <div class="icon d-flex align-items-center">
                                         <i class="feather-user h6 me-2 mb-0"></i>
                                         <span>Sign in</span>
